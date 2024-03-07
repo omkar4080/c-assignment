3078f88731c53e6922386e5db361b74537f251ca
omkar <omkarudavant133@gmail.com>
2024-03-07 17:56:47 +0530

    ok

---
+++ assi12q.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+ int num;
+ printf("enter number:\n");
+ scanf("%d",&num);
+ printf("char value=%c\n",num);
+ printf(" \n octal value=%o",num);
+ printf("\n decimal value=%d",num);
+ printf("\n hex deci value=%x",num);
+ return 0;
+}

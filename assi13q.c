a3ad149c727561067ff0de230f94cdee6297da7f
omkar <omkarudavant133@gmail.com>
2024-03-07 23:10:35 +0530

    ready

---
+++ assi13q.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{  
+
+	char a;
+	printf("\n entr char value:");
+	scanf("%c",&a);
+	printf("\n ascii value=%d",a);
+	printf("\n ocatal value=%o",a);
+	printf("\n hex value=%x",a);
+	printf(" decimal value=%d\n ",a);
+
+	int b;
+	printf(" enter ascii value:");
+	scanf("%d",&b);
+	printf(" char value=%c\n",b);
+
+	return 0;
+}

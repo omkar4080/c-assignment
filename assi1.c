127353d898101feb531376f1addac5e1558e9e6e
omkar <omkarudavant133@gmail.com>
2024-03-07 17:44:23 +0530

    new

---
+++ assi1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{  
+
+int o;
+int r;
+int a;
+
+printf("enter frist number:\n");
+scanf("%d",&o);
+printf("enter second number:\n");
+scanf("%d",&r);
+a = o + r;
+printf("add two number=%d\n",a);
+printf(".............................................\n");
+
+int b;
+b = o-r;
+printf("sub two number=%d\n",b);
+printf("\n...........................................");
+
+int c;
+c = o*r;
+printf("mul two number=%d\n",c);
+
+return 0;
+}
